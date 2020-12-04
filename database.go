6d131ece21fcc6e576b1ef44713c2cb427032034
Paul Greenberg <greenpau@outlook.com>
2020-12-03 22:02:07 -0500

    released v1.0.11

--- database.go
+++ database.go
@@ -37,9 +37,9 @@ func init() {
 	app = versioned.NewPackageManager("go-identity")
 	app.Description = "go-identity"
 	app.Documentation = "https://github.com/greenpau/go-identity"
-	app.SetVersion(appVersion, "1.0.10")
+	app.SetVersion(appVersion, "1.0.11")
 	app.SetGitBranch(gitBranch, "master")
-	app.SetGitCommit(gitCommit, "v1.0.9-2-g262d111")
+	app.SetGitCommit(gitCommit, "v1.0.10-2-gf233402")
 	app.SetBuildUser(buildUser, "")
 	app.SetBuildDate(buildDate, "")
 }
